bb393bcf602a75067e73070bed7359578e581935
McAr0v <dvijkz@gmail.com>
2023-12-25 12:12:03 +0600

    Добавьте файлы проекта.

---
+++ ConsoleAppSearchUtility/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FileTextSearchUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Порядок ввода значений: <путь> <формат> <текст для поиска>");
+                return;
+            }
+
+            var files = Task2.FileSearchUtility.SearchFiles(args[0], args[1]);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Поиск по файлу: {file}");
+                Task3.TextSearchUtility.SearchAndPrint(file, args[2]);
+                Console.WriteLine(new string('-', 50));
+            }
+
+            Console.WriteLine("Нажмите любую клавишу чтобы продолжить...");
+            Console.ReadLine();
+        }
+    }
+}
